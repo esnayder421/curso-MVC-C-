21fb9d138ff57fc8adcd65be9871f4061bcac89f
esnayder421 <esnayder421@gmail.com>
2023-02-04 19:54:50 -0500

    first commit

---
+++ CapaDatos/CD_Usuarios.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+using System.Data.SqlClient;
+using System.Data;
+
+// HACEMOS LLAMADOS AL COSTRUCTOR (USUARIO) PARA ENVIAR Y SACAR DATOS
+
+namespace CapaDatos
+{
+    public class CD_Usuarios
+    {
+        //-------------------------------------------------LISTAR---------------------------------------------------------
+        public List<Usuario> Listar()
+        {
+            List<Usuario> lista = new List<Usuario>();
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn)) {
+
+                    string query = "SELECT IdUsuario,Nombres,Apellidos,Correo,Clave,Reestablecer,Activo FROM USUARIO";
+                    SqlCommand cmd = new SqlCommand(query, oconexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader()) {
+
+                        while (dr.Read())
+                        {
+                            lista.Add(
+                                new Usuario()
+                                {
+                                    IdUsuario = Convert.ToInt32(dr["IdUsuario"]),
+                                    Nombres = dr["Nombres"].ToString(),
+                                    Apellidos = dr["Apellidos"].ToString(),
+                                    Correo = dr["Correo"].ToString(),
+                                    Clave = dr["Clave"].ToString(),
+                                    Reestablecer = Convert.ToBoolean(dr["Reestablecer"]),
+                                    Activo = Convert.ToBoolean(dr["Activo"])
+                                }) ;
+                        }
+                    }
+                
+                }
+
+            }
+            catch 
+            {
+
+                lista = new List<Usuario>();
+            }
+
+
+
+            return lista;
+        }
+        //----------------------------------------------------------------------------------------------------------------
+
+
+
+        //------------------------------------------------------INSERTAR-------------------------------------------------------
+
+        public int Registrar (Usuario obj, out string Mensaje)
+        {
+            int idautogenerado = 0;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_RegistrarUsuario", oconexion);  /*aqui llamamos el procedimiento almacenado*/
+                    //---------------------Hacemos el llamado a todos los parametros de entrada--------------------
+                    cmd.Parameters.AddWithValue("Nombres", obj.Nombres);
+                    cmd.Parameters.AddWithValue("Apellidos", obj.Apellidos);
+                    cmd.Parameters.AddWithValue("Correo", obj.Correo);
+                    cmd.Parameters.AddWithValue("Clave", obj.Clave);
+                    cmd.Parameters.AddWithValue("Activo", obj.Activo);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output; //parametro de salida desde la base de datos
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output; //parametro de salida 
+                    cmd.CommandType = CommandType.StoredProcedure; // especificamos el tipo de comando a ejecutar(procedimiento)
+                    //------------------------------------------------------------------------------------------------
+                    oconexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    idautogenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                idautogenerado = 0;
+                Mensaje = ex.Message;
+                
+            }
+            return idautogenerado;
+
+
+        }
+
+        //----------------------------------------------------------------------------------------------------------------
+
+
+        //--------------------------------------------------------ACTUALIZAR-----------------------------------------------
+
+        public bool Editar(Usuario obj, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_EditarUsuario", oconexion);  /*aqui llamamos el procedimiento almacenado*/
+                    //---------------------Hacemos el llamado a todos los parametros de entrada--------------------
+                    cmd.Parameters.AddWithValue("IdUsuario", obj.IdUsuario);
+                    cmd.Parameters.AddWithValue("Nombres", obj.Nombres);
+                    cmd.Parameters.AddWithValue("Apellidos", obj.Apellidos);
+                    cmd.Parameters.AddWithValue("Correo", obj.Correo);
+                    //cmd.Parameters.AddWithValue("Clave", obj.Clave);
+                    cmd.Parameters.AddWithValue("Activo", obj.Activo);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output; //parametro de salida desde la base de datos
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output; //parametro de salida 
+                    cmd.CommandType = CommandType.StoredProcedure; // especificamos el tipo de comando a ejecutar(procedimiento)
+                    //------------------------------------------------------------------------------------------------
+                    oconexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+
+            }
+            return resultado;
+
+
+        }
+
+        public bool Eliminar (int id, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("delete top (1) from usuario where IdUsuario = @id", oconexion);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.CommandType = CommandType.Text;
+                    oconexion.Open();
+
+                    resultado = cmd.ExecuteNonQuery() > 0 ? true : false;
+                }
+            }
+            catch (Exception ex )
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+//de aqui nos vamos para la capa de negocio
---
+++ CapaPresentacionAdmin/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+using CapaNegocio;
+using CapaEntidad;
+
+//los datos vienen de CAPA DE NEGOCIO
+
+namespace CapaPresentacionAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Usuarios()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ListarUsuarios()
+        {
+            List<Usuario> oLista = new List<Usuario>();
+
+             oLista = new CN_Usuarios().Listar();
+
+                
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+
+
+        }
+
+        [HttpPost]
+        public JsonResult GuardarUsuario(Usuario objeto)
+        {
+            object resultado; // permite guardar cualquier tipo de valor 
+            string mensaje = string.Empty;
+
+            if (objeto.IdUsuario == 0)
+            {
+                // llamamos la capa de negocio y instanciamos la clase registrar  
+                resultado = new CN_Usuarios().Registrar(objeto, out mensaje);
+
+            }
+            else
+            {
+                resultado = new CN_Usuarios().Editar(objeto, out mensaje);
+
+            }
+
+
+            return Json(new {resultado = resultado, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+
+        }
+
+
+        [HttpPost]
+        public JsonResult EliminarUsuario(int id)
+        {
+            bool respuesta = false;
+            string mensaje = string.Empty;
+
+
+            respuesta = new CN_Usuarios().Eliminar(id, out mensaje);
+
+            return Json(new { resultado = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+    }
+}
